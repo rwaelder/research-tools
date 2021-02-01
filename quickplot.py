33b7ed54609dbd31f18b7d88f53910ce5bb690c8
rwaelder <rwwaelde@buffalo.edu>
2021-02-01 16:56:59 -0500

    Added argparse controls

--- quickplot.py
+++ quickplot.py
@@ -1,5 +1,5 @@
 '''
-Requirements: matplotlib, numpy
+Requirements: matplotlib, numpy, argparse
 
 Function for quick visualization of 2D data, ignores headers, 
 automatically handles comma, space, and tab delimiters, 
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 import sys
+import argparse
 
 # ---------- Helper functions ----------------------------------
 
@@ -63,46 +64,79 @@ def is_number(value):
 
 # ---------- Program Functions ---------------------------------
 
+def offset_data(data, offset):
+	return [datum+offset for datum in data]
+
 # ---------- Main Program --------------------------------------
 
-xlabel = input('X label:\n')
-ylabel = input('Y label:\n')
-title = input('Title:\n')
-if title == '':
-	title = '-'.join(sys.argv[1].split('.')[:-1])
+def main(files, options):
 
-style = input('Line, dot, or both?\n')
-
-colors = cm.gist_rainbow( np.linspace(0, 1, len(sys.argv)-1) )
-
-legend = []
-for i, filename in enumerate(sys.argv[1:]):
-
-	legendFilename = '.'.join( filename.split('.')[:-1] )
-	legend.append(legendFilename)
-
-	x = []
-	y = []
-
-	x, y = read_2d_file(filename)
-
-	if style == '' or style.lower()[0] == 'line'[0]:
-		plt.plot(x,y, color=colors[i])
-
-	elif style.lower()[0] == 'dot'[0]:
-		plt.plot(x,y,'o', color=colors[i])
-
-	elif style.lower()[0] == 'both'[0]:
-		plt.plot(x,y,'-o', color=colors[i])
-
+	if not options.title:
+		title = '-'.join(sys.argv[1].split('.')[:-1])
 	else:
-		plt.plot(x,y, color=colors[i])
+		title = options.title
 
-plt.xlabel(xlabel)
-plt.ylabel(ylabel)
-plt.title(title)
+	style = options.style
 
-if len(legend) > 1:
-	plt.legend(legend)
 
-plt.show()
+	colorspace = cm.get_cmap(name=options.color)
+	colors = colorspace( np.linspace(0, 1, len(files)) )
+
+	legend = []
+	for i, file in enumerate(files):
+
+		# remove extension
+		legendFilename = '.'.join( file.split('.')[:-1] )
+		# slice based on optional arguments
+		legendFilename = legendFilename[options.legend_start_cutoff:options.legend_end_cutoff]
+		legend.append(legendFilename)
+
+		x = []
+		y = []
+
+		x, y = read_2d_file(file)
+
+		if options.offset != 0:
+			offset = options.offset * i
+			y = offset_data(y, offset)
+
+		if style == '' or style.lower()[0] == 'line'[0]:
+			plt.plot(x,y, color=colors[i])
+
+		elif style.lower()[0] == 'dot'[0]:
+			plt.plot(x,y,'o', color=colors[i])
+
+		elif style.lower()[0] == 'both'[0]:
+			plt.plot(x,y,'-o', color=colors[i])
+
+		else:
+			plt.plot(x,y, color=colors[i])
+
+	plt.xlabel(options.xlabel)
+	plt.ylabel(options.ylabel)
+	plt.title(title)
+
+	if len(legend) > 1:
+		plt.legend(legend)
+
+	plt.show()
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Generate a quick plot of 2D data.')
+	parser.add_argument('data_files', nargs='+', help='Data files to plot')
+
+	parser.add_argument('-x', '--xlabel', type=str, help='x axis label', default='')
+	parser.add_argument('-y', '--ylabel', type=str, help='y axis label', default='')
+	parser.add_argument('-t', '--title', type=str, help='plot title', default='')
+	parser.add_argument('-s', '--style', type=str, help='plot as line, dot, or both', choices=['line', 'dot', 'both'], default='line')
+	parser.add_argument('-o', '--offset', type=float, help='vertical offset between datasets', default=0)
+	parser.add_argument('-c', '--color', type=str, help='color palette for plots. see https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html', default='gist_rainbow')
+	parser.add_argument('-k', '--legend_start_cutoff', type=int, help='character to start cutoff of filenames for legend', default=0)
+	parser.add_argument('-l', '--legend_end_cutoff', type=int, help='character to end cutoff of filenames for legend', default=None)
+
+	args = parser.parse_args()
+
+	files = args.data_files
+
+	main(files, args)
